df01dba961c161b4be0ec51680cbb017537ecfa5
Júlia S. Coité <juliascoite@gmail.com>
2025-06-17 19:49:26 -0300

    adicionando mensagem de erro no HTML

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 async function fetchData(){
 
+    const imgElement = document.getElementById("pokemonSprite");
+    const errorMsg = document.getElementById("errorMsg");
+
     try{
 
         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
 
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
 
         if (!response.ok) {
             throw new Error("Could not fetch resource");
@@ -12,13 +16,16 @@ async function fetchData(){
 
         const data = await response.json();
         const pokemonSprite = data.sprites.front_default;
-        const imgElement = document.getElementById("pokemonSprite");
 
         imgElement.src = pokemonSprite;
         imgElement.style.display = "block";
+        errorMsg.style.display = "none";
 
     }
     catch(error){
         console.log(error);
+
+        imgElement.style.display = "none";
+        errorMsg.style.display = "block";
     }
 }
